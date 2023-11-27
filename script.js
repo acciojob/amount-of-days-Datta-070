d017890b85473642f50c6ffc3704ce5c82188cf6
Chinmay Datt D <chinmaydatt003@gmail.com>
2023-11-27 12:14:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function daysOfAYear(date){
+	if(date%4 == 0 && date%100 != 0 || date%100 == 0){
+		return 366;
+	}
+	return 365;
+}
+let date = prompt("Enter the Year");
+alert(daysOfAYear(date));
