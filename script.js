ddce614d88cbfa8aef42776f92b2692c7222b67d
Chinmay Datt D <chinmaydatt003@gmail.com>
2023-11-27 12:16:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 function daysOfAYear(date){
-	if(date%4 == 0 && date%100 != 0 || date%100 == 0){
+	if(date%4 == 0 && date%100 != 0 || date%400 == 0){
 		return 366;
 	}
 	return 365;
